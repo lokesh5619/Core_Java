9373d03b0e63e09b74a68ce74cdf0013a12ca9fc
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2025-12-11 00:19:26 +0700

    new

--- primeNumber.java
+++ primeNumber.java
@@ -3,25 +3,24 @@ class primeNumber
 {	
 	public static void main(String ar[])
 	{ 
-	int n=16;
-	int i;
-	int c=1;
-	for(i=2; i<n; i++)
-	{
-		if(n%i==0)
-		{
-			c=0;
-		}
+	    int n=16;
+	    int i;
+	    int c=1;
+	    for(i=2; i<n; i++)
+	    {
+	    	if(n%i==0)
+		    {
+		    	c=0;
+		    }
+	    }
+	    if(c==1)
+	    {
+	    	System.out.println("no. is prime");
+	    }
+	    else
+	    {
+	    	System.out.println("no. is not prime");
+	    }
 	}
-	if(c==1)
-	{
-		System.out.println("no. is prime");
-	}
-	else
-	{
-		System.out.println("no. is not prime");
-	}
-	}
-	
 }
 	
