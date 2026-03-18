0b347e8854952764158adfb65964493e371fcf05
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2026-03-18 14:23:03 +0700

    exception

--- Exception handling/checkedException.java
+++ Exception handling/checkedException.java
@@ -1,8 +1,9 @@
 // checked exception
 import java.io.*;
+import java.io.FileNotFoundException;
 class Demo9
 {
-	static public void main(String ar[]) // 10 2 , 10 0
+	static public void main(String ar[]) throws FileNotFoundException
 	{
 		System.out.println("softwaves__1");
 		PrintWriter pw=new PrintWriter("xyz.txt");
